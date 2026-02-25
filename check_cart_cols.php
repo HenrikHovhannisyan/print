1e0b98691d75d63f0e7f067b3a1692074c500761
Henrik <henrik.hovhannisyan.91@gmail.com>
2026-02-25 13:48:20 +0400

    Cleanup: remove temporary/debug files and add .gitignore

--- check_cart_cols.php
+++
@@ -1,7 +0,0 @@
-<?php
-$dbFile = __DIR__ . '/data/printeditor.db';
-$db = new PDO('sqlite:' . $dbFile);
-$res = $db->query("PRAGMA table_info(cart_items)")->fetchAll(PDO::FETCH_ASSOC);
-foreach ($res as $row) {
-    echo $row['name'] . "\n";
-}
--- check_order_items_cols.php
+++
@@ -1,7 +0,0 @@
-<?php
-require 'backend/bootstrap.php';
-$db = Database::getInstance();
-$columns = $db->fetchAll("PRAGMA table_info(order_items)");
-foreach ($columns as $c) {
-    echo $c['name'] . "\n";
-}
